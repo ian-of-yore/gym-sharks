35484335908c1481dda97f2e10ee678386b15b76
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-09-28 23:02:46 +0600

    user info done

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import user from '../../images/user.jpg'
 import './User.css'
 
 const User = () => {
     return (
         <div className='user-container'>
-            <h1>User Data</h1>
+            <div className='user-profile'>
+                <div>
+                    <img src={user} alt="" />
+                </div>
+                <div>
+                    <h2>Yellow Submarine</h2>
+                    <h4>Liverpool, England</h4>
+                </div>
+            </div>
+            <div className='user-info'>
+                <div className='user-info-details'>
+                    <div><div className='user-weight'>
+                        <h1>72</h1>
+                        <p> Kg</p>
+                    </div>
+                        <h3>Weight</h3></div>
+                </div>
+                <div className='user-info-details'>
+                    <div><div className='user-weight'>
+                        <h1>5.9</h1>
+
+                    </div>
+                        <h3>Height</h3></div>
+                </div>
+                <div className='user-info-details'>
+                    <div><div className='user-weight'>
+                        <h1>26</h1>
+                        <p> Yrs</p>
+                    </div>
+                        <h3>Age</h3></div>
+                </div>
+
+            </div>
         </div>
     );
 };
